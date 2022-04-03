c5bf36081208f86840bb185a5a646a57d2ec7922
Lautaro Fullone <66123086+LautaroFullone@users.noreply.github.com>
2022-04-02 23:51:20 -0300

    Adding all necesary login logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginRegisterComponent } from './components/login-register/login-register.component';
+import { HttpClientModule} from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginRegisterComponent
+    LoginRegisterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/util/util-constants.ts
@@ -0,0 +1,4 @@
+export class Util_Constants {
+
+    public static API_URL: String = 'http://localhost:8080/';
+}
