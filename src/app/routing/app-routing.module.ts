183594583973cde99f074df0268c81c95740fec6
Lautaro Fullone <66123086+LautaroFullone@users.noreply.github.com>
2022-04-02 23:54:58 -0300

    Adding all necesary login logic (#5)

--- src/app/routing/app-routing.module.ts
+++ src/app/routing/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from '../components/dashboard/dashboard.component';
 import { LoginRegisterComponent } from '../components/login-register/login-register.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginRegisterComponent },
+  { path: 'dashboard', component: DashboardComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' }
   
 ];
