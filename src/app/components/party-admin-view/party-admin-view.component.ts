b3ef8ec069f6322706cbc78fee4fb2cd111a81d8
Lautaro Fullone <66123086+LautaroFullone@users.noreply.github.com>
2022-04-20 23:17:05 -0300

    Creating switcher party view component demo

---
+++ src/app/components/party-admin-view/party-admin-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-party-admin-view',
+  templateUrl: './party-admin-view.component.html',
+  styleUrls: ['./party-admin-view.component.css']
+})
+export class PartyAdminViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
