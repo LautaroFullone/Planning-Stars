c4aeede578cf65d1f99e1bc6e29353f45c619494
Lautaro Fullone <66123086+LautaroFullone@users.noreply.github.com>
2022-04-30 00:23:16 -0300

    First responsive version done

--- src/app/components/party-admin-view/party-admin-view.component.ts
+++ src/app/components/party-admin-view/party-admin-view.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PartyAdminViewComponent implements OnInit {
 
+  isItemSelected: Boolean = true;
+
   constructor() { }
 
   ngOnInit(): void {
