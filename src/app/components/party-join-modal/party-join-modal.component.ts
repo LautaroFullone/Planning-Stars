7ac09dfdf8e6ee3c1582f6316d677873a0a04da5
Lautaro Fullone <66123086+LautaroFullone@users.noreply.github.com>
2022-06-12 16:20:34 -0300

    PS-120: F_Join Party modal should connect user into socket party (#29)
    last changes

--- src/app/components/party-join-modal/party-join-modal.component.ts
+++ src/app/components/party-join-modal/party-join-modal.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgToastService } from 'ng-angular-popup';
+import { PartyService } from 'src/app/services/party.service';
 
 @Component({
-  selector: 'app-party-join-modal',
-  templateUrl: './party-join-modal.component.html',
-  styleUrls: ['./party-join-modal.component.css']
+    selector: 'app-party-join-modal',
+    templateUrl: './party-join-modal.component.html',
+    styleUrls: ['./party-join-modal.component.css']
 })
 export class PartyJoinModalComponent implements OnInit {
 
-  constructor() { }
+    partyForm = new FormGroup({
+        id: new FormControl('', [Validators.required]),
+    });
 
-  ngOnInit(): void {
-  }
+    get id() { return this.partyForm.get('id').value; }
+
+    constructor(private partyService: PartyService,
+                private router: Router,
+                private toast: NgToastService) { }
+
+    ngOnInit(): void {
+        
+    }
+
+    ngSubmit() {
+        this.partyService.getPartyByID(this.id).subscribe({
+            next: () => {   this.router.navigateByUrl(`/party/${this.id}`)  },
+            error: (apiError) => {
+                this.toast.error({
+                    detail: apiError.error.message,
+                    summary: apiError.error.errors[0],
+                    position: 'br', duration: 6000
+                })
+            }
+        })
+    }
 
 }
