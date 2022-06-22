219515a66dee7a1e3e7947999902c0f740a37353
Lautaro Fullone <66123086+LautaroFullone@users.noreply.github.com>
2022-06-22 17:03:53 -0300

    PS-143-v2: F_When admin select a US, players should see same item into the screen (#42)
    selected us improvement

--- src/app/components/party-player-view/party-player-view.component.ts
+++ src/app/components/party-player-view/party-player-view.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { UserStory } from 'src/app/models/user-story';
-import { SocketWebService } from 'src/app/services/socket-web.service';
 
 @Component({
     selector: 'app-party-player-view',
@@ -10,31 +9,9 @@ import { SocketWebService } from 'src/app/services/socket-web.service';
 export class PartyPlayerViewComponent implements OnInit {
 
     @Input() partyID: string;
+    @Input() actualUserStory: UserStory;
 
-    actualUserStory: UserStory /*= {
-    "id": 76,
-    "tag": "16F9T",
-    "name": "F_PartySwich component should validate if the party exists",
-    "description": "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. \nLorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. \nLorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. ",
-    "storyPoints": null,
-    "sprint": "8",
-    "workArea": "FrontEnd",
-    "storyWritter": "Lautaro Fullone",
-    "fileLink": "http://givemenbastreams.com/nba/heat-live-stream?sport=basketball",
-    "isActive": true
-  }*/
-    constructor(private socketService: SocketWebService) { }
+    constructor() { }
 
-    ngOnInit(): void {
-        this.listenServerEvents();
-    }
-
-    listenServerEvents() {
-        this.socketService._selectedUS.subscribe({
-            next: (userStory) => {
-                console.log('socket us',userStory);
-                this.actualUserStory = userStory;
-            }
-        })
-    }
+    ngOnInit(): void { }
 }
