eec557f5c8d661893cfee237949a979110eae0b7
Lautaro Fullone <66123086+LautaroFullone@users.noreply.github.com>
2022-06-24 18:10:28 -0300

    adding votation service/model

---
+++ src/app/services/votation.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Votation } from '../models/votation';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class VotationService {
+
+    private headers = {
+        headers: new HttpHeaders({
+            'Content-Type': 'application/json'
+        })
+    };
+
+    constructor(private http: HttpClient) { }
+
+    createVotation(votation: Votation, userStoryID: string): Observable<any> {
+        return this.http.put<any>(`${environment.apiURL}/userStory/${userStoryID}/votation`, votation, this.headers);
+    }
+
+    getPartyPlayers(userStoryID: string): Observable<any> {
+        return this.http.get<any>(`${environment.apiURL}/userStory/${userStoryID}/votations`, this.headers);
+    }
+}
