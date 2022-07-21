a7cf026a95e9b52c4c17191999feef241bf6aec9
Lautaro Fullone <66123086+LautaroFullone@users.noreply.github.com>
2022-07-21 18:45:27 -0300

    event changes

--- src/app/components/party-player-view/party-player-view.component.ts
+++ src/app/components/party-player-view/party-player-view.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { UserStory } from 'src/app/models/user-story';
+import { NotificationService } from 'src/app/services/notification.service';
 import { SocketWebService } from 'src/app/services/socket-web.service';
 
 @Component({
@@ -12,12 +13,18 @@ export class PartyPlayerViewComponent implements OnInit {
     @Input() partyID: string;
     actualUserStory: UserStory;
 
-    constructor(private socketService: SocketWebService) { }
+    constructor(private socketService: SocketWebService,
+                private toast: NotificationService) { }
 
     ngOnInit(): void {
         this.socketService.selectedUS$.subscribe({
             next: (userStory) => {
                 this.actualUserStory = userStory;
+
+                this.toast.infoToast({
+                    title: 'New Item selected',
+                    description: 'Admin has selected a US'
+                })
             }
         })
     }
