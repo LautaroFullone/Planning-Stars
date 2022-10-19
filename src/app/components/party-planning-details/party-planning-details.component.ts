7f3179c2f3235d74718e04391198b8f77cfe36b3
Lautaro Fullone <66123086+LautaroFullone@users.noreply.github.com>
2022-10-19 16:13:55 -0300

    PS-184: F_Show story points into user stories list (#77)
    * us-list updates
    
    * changes

--- src/app/components/party-planning-details/party-planning-details.component.ts
+++ src/app/components/party-planning-details/party-planning-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { UserStory } from 'src/app/models/user-story';
+import { NotificationService } from 'src/app/services/notification.service';
 import { SocketWebService } from 'src/app/services/socket-web.service';
 import { VotationService } from 'src/app/services/votation.service';
 
@@ -15,28 +16,41 @@ export class PartyPlanningDetailsComponent implements OnInit, OnDestroy {
     @Input() userType: string;
     @Input() selectedUS: UserStory;
 
+    usPlanning: UserStory
     planningResults = undefined;
+    planningGoing: boolean = false;
 
     private planningConcludedSub: Subscription;
+    private planningStartedSub: Subscription;
     private numberOfUsersSub: Subscription;
 
     constructor(private socketService: SocketWebService,
+                private toast: NotificationService,
                 private votationService: VotationService) { }
 
-    ngOnInit(): void {
+
+    ngOnInit(): void { 
+        this.planningStartedSub = this.socketService.planningStarted$.subscribe({
+            next: (us) => {
+                this.usPlanning = us;
+                this.planningGoing = true;
+            }
+        }) 
+
         this.planningConcludedSub = this.socketService.plannigConcluded$.subscribe({
             next: (data) => {
-                console.log('plannigConcluded',data)
+                // console.log('plannigConcluded',data)
                 let userStory = data.userStory;
+                this.planningGoing = false;
 
                 this.numberOfUsersSub = this.socketService.getnumberOfConnectedUsersIntoParty().subscribe({
                     next: (numberOfUsers) => {
-                        console.log('getnumberOfConnectedUsersIntoParty', numberOfUsers);
+                        // console.log('getnumberOfConnectedUsersIntoParty', numberOfUsers);
                         this.numberOfUsersSub.unsubscribe();
 
                         this.votationService.getPlanningDetails(userStory.id, (numberOfUsers-1)).subscribe({
                             next: (details) => {
-                                console.log(details)
+                                console.log('getPlanningDetails',details);
                                 
                                 this.planningResults = {
                                     votationsReceived: details.userVotes.length,
@@ -51,7 +65,13 @@ export class PartyPlanningDetailsComponent implements OnInit, OnDestroy {
                                         user: details.minVote.name,
                                     }
                                 }
-                                console.log('planningResults', this.planningResults);
+                                // console.log('planningResults', this.planningResults);
+                            },
+                            error: (apiError) => {
+                                this.toast.errorToast({
+                                    title: apiError.error.message,
+                                    description: apiError.error.errors[0]
+                                })
                             } 
                         })
                     }
@@ -65,6 +85,10 @@ export class PartyPlanningDetailsComponent implements OnInit, OnDestroy {
         this.planningConcludedSub.unsubscribe();
     }
 
+    get isPlanningUsSelected() {
+        return (this.planningGoing && this.selectedUS && this.usPlanning && this.selectedUS.id == this.usPlanning.id);
+    }
+    
     get isOwner() {
         return this.userType == 'owner';
     }
